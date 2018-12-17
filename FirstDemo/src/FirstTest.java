cfb7eaca8270e0cf9542ad5ece393262e9dc9794
rp0001 <rp0001@WL0990-0049102.bbby.corp.int>
2018-12-17 09:15:24 -0500

    First day commit

---
+++ FirstDemo/src/FirstTest.java
@@ -0,0 +1,10 @@
+
+public class FirstTest {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Test1");
+
+	}
+
+}
---
+++ FirstDemo/src/ThirdTest.java
@@ -0,0 +1,10 @@
+
+public class ThirdTest {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Test3");
+
+	}
+
+}
